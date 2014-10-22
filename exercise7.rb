c2702f7e5b578dde2347a8d49b3ae732e5e9e488
Kiko <mmhabib11@gmail.com>
2014-10-22 16:08:18 -0400

    Changed how i got the sum of the cohorts to use inject

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,28 @@
+# def display_hash
+# 	students = {
+# 	:cohort1 => 34,
+# 	:cohort2 => 42,
+# 	:cohort3 => 22
+# }
+# 	students[:cohort4]= 43
+# 	students.delete(:cohort2)
+# 	students.each do |key, value|
+# 		puts "#{key}: #{value}"
+# 	end
+# 	puts "* #{students.keys}"
+# 	puts "* After expanding the cohorts 5%. These are the new values (rounded)"
+# 	students.each do |key, value|
+# 		puts "#{value + (value * 0.05).round}"
+# 	end
+# 	sum = 0
+# 	students.each_value do |value|
+# 		sum = sum + value
+# 	end
+# 	puts "Total sum of cohorts = #{sum}"
+# end
+
+# display_hash
+
 def display_hash
 	students = {
 	:cohort1 => 34,
@@ -14,11 +39,7 @@ def display_hash
 	students.each do |key, value|
 		puts "#{value + (value * 0.05).round}"
 	end
-	sum = 0
-	students.each_value do |value|
-		sum = sum + value
-	end
-	puts "Total sum of cohorts = #{sum}"
+	puts "Total sum of cohorts = #{students.values.inject{|sum, n| sum + n}}"
 end
 
 display_hash
